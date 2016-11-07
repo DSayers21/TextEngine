bdae2fc0d71752c6c2c88962ea1463414af7722c
DSayers21 <dsayers77@gmail.com>
2016-11-07 14:24:22 +0000

    More Comments

--- TextEngine/InputControl.h
+++ TextEngine/InputControl.h
@@ -22,29 +22,29 @@ namespace TxtEgn
 		std::vector<std::string> AskInput();
 		//Takes a vector of strings and combines the strings in it after a start positon in the vector
 		std::string ParseIntoSentence(std::vector<std::string> TempVector, int Start);
-
+		//Returns how many letters that are im a 100 character input
 		int HowManyLetters(char input[100]);
-
+		//Checks if a string is a number
 		bool is_number(std::string& s);
-
+		//Combines two strings together
 		std::string CombineString(std::string Combi1, std::string Combi2);
-
+		//Finds if a string is located in a vector of strings
 		bool FindString(std::vector<std::string> CheckVec, std::string Search);
-
+		//Gets the location of a string inside of a vector of strings
 		int FindStringPosition(std::vector<std::string> CheckVec, std::string Search);
-
+		//Compares two strings and says if they are 
 		bool CompareStrings(std::string One, std::string Two);
-
+		//Aligns a string to the center
 		std::string AlignCenter(char Letter, int Buffer, std::string Original);
-
+		//Aligns a string to the left
 		std::string AlignLeft(char Letter, int Buffer, std::string Original);
-
+		//Aligns a string to the right
 		std::string AlignRight(char Letter, int Buffer, std::string Original);
-
+		//Parses a input string by commas and returns a vector of ints
 		std::vector<int> ParseByComma(std::string InputS);
-
+		//Searches a vector of strings to find the location of a string within a vector of strings
 		int SearchVector(std::vector<std::string> Vector, std::string Find);
-
+		//Removes all spaces in a string
 		std::string RemoveSpaces(std::string Original);
 	};
 }
